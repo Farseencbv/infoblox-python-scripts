69067e7ad9e0e93adc2d563934e2a15be1cc62d7
Farseencbv <42977873+Farseencbv@users.noreply.github.com>
2024-04-23 17:30:27 +0530

    updated the entire script

--- export_backup/export_backup.py
+++ export_backup/export_backup.py
@@ -1,54 +1,65 @@
+#!/usr/bin/python3.8
 """
 This script downloads an Infoblox database backup and saves it to the local file system. 
 It connects to an Infoblox Grid Manager using the RESTfulAPI and authenticates using the provided credentials. 
 The backup file is downloaded using the URL received from the response of initiating the backup session. 
-The file is then saved locally with a timestamped filename. 
-Finally, a POST request is sent to trigger the download complete using the received token.
 
 """
 
-
-import requests
-import json
 import time
+import json
+import requests
+import urllib3
+from dotenv import load_dotenv
+import os
 
 # Disable SSL warnings
-requests.packages.urllib3.disable_warnings()
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-# Grid variables
-gm_url = "https://1.1.1.1/wapi/v2.12.2"
-gm_user = 'fars'
-gm_pwd = 'infoblox'
-now = time.strftime('%Y-%m-%d')
-outputfile = f'infoblox_backup_{now}.bak'
+# Grid Creds
+load_dotenv('.env')
+gm_user = os.getenv('INFOBLOX_USERNAME')
+gm_pwd  = os.getenv('INFOBLOX_PASSWORD')
 
+
+# Grid Variables
+gm_url = "https://gridmaster/wapi/v2.12.2"
+now = time.strftime('%Y-%m-%d_%I-%M_%p')
+outputfile = f'FNF_Infoblox_Backup_{now}.bak'
 
 # Setting up a session with the Infoblox GM
 s = requests.Session()
 s.auth = (gm_user, gm_pwd)
-s.headers = {"content-type": "application/json"}
+s.headers = {"content-type" : "application/json"}
 s.verify = False
 
-def infoblox_backup():
-    """Function to download Infoblox database backup"""
+# Function to get Backup
 
+def infoblox_backup():
+    """
+    Function to download Infoblox database backup 
+    Raises:
+        requests.exceptions.RequestException: If any step (initiating backup, downloading file, or completing backup) fails.
+    
+    """
 
     # Step 1: Initiate Grid Backup Session
-    backup = {"type": "BACKUP"}
-    response = s.post(f'{gm_url}/fileop?_function=getgriddata', data=json.dumps(backup))
-    
+    initial_url = f"{gm_url}/fileop?_function=getgriddata"
+    backup = {'type': 'BACKUP', 'discovery_data': False}
+    response = s.post(base_url, data=json.dumps(backup))
+
     if not response.ok:
-        raise Exception(f"Grid backup initiation failed with HTTP error code {response.status_code}")
-    
-    response_data = response.json()
-    token = response_data['token']
-    url = response_data['url']
+        raise requests.exceptions.RequestException(f"Grid backup initiation failed with HTTP error code {response.status_code}")
 
+    # Extract token and url from the response
+    output = response.json()
+    token = output['token']
+    url =  output['url']
 
-    # Step 2: Download backup file using the url
+     # Step 2: Download backup file using the url
     download_headers = {"content-type": "application/force-download"}
-    download_file = s.get(url, stream=True, headers=download_headers)
-    
+    download_file = s.get(url, headers=download_headers, stream=True)
+
     if not download_file.ok:
         raise requests.exceptions.RequestException(f"Downloading backup file failed with HTTP error code {download_file.status_code}")
 
@@ -56,19 +67,18 @@ def infoblox_backup():
     with open(outputfile, 'wb') as file:
         file.write(download_file.content)
 
-    print(f"The backup has been downloaded as '{outputfile}'")
+    print(f"The backup has been downloaded as {outputfile}")
 
-
-    # Step 4: Post a call to trigger download complete using the received token from the above steps
+     # Step 4: Post a call to trigger download complete using the received token from the above steps
+    final_url = f"{gm_url}/fileop?_function=downloadcomplete"
     backup_token = {"token": token}
-    backup_complete = s.post(f'{gm_url}/fileop?_function=downloadcomplete', data=json.dumps(backup_token))
+    backup_complete = s.post(final_url, data=json.dumps(backup_token))
     
     if not backup_complete.ok:
-       raise requests.exceptions.RequestException(f"Download token deletion failed with HTTP error code {backup_complete.status_code}")
-    
+        raise requests.exceptions.RequestException(f"Infoblox backup didn't complete as expected with HTTP error code {backup_complete.status_code}")
+
 
 
 if __name__ == '__main__':
     infoblox_backup()
-
-    
+   
